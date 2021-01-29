4c30db0d213c11880e9852fc599ea9c3ceef399c
Nader <na511dr@hotmail.com>
2021-01-29 19:01:13 +0300

    dynamic typing using the def keyword

--- src/AST.groovy
+++ src/AST.groovy
@@ -1,6 +1,14 @@
 class AST {
     static void main(String[] args) {
-        println("Nader is here")
-        
+        def age = 10
+        def name = "Nader"
+        def is_active = false
+        println(age)
+        println(name)
+        println(is_active)
+
+        println(age.class.typeName)
+        println(name.class.typeName)
+        println(is_active.class.typeName)
     }
 }
