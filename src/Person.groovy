341bdb5c7b28dcd8f84d6edb19a50b7d48d29011
Nader <na511dr@hotmail.com>
2021-01-29 19:26:25 +0300

    dynamic typing using the def keyword

---
+++ src/Person.groovy
@@ -0,0 +1,10 @@
+class Person {
+    String firstName;
+    String lastName;
+    int age;
+    String fullName = lastName + ", "+ firstName;
+
+    String getName(){
+        return lastName + ", "+ firstName;
+    }
+}
