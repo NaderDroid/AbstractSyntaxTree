b722f5e179c3acd184c68cc37459a5ba085e6b9c
Nader <na511dr@hotmail.com>
2021-01-29 23:45:14 +0300

    iteratives in groovy

--- src/AST.groovy
+++ src/AST.groovy
@@ -1,10 +1,7 @@
+import org.apache.tools.ant.taskdefs.Echo
+
 class AST {
     static void main(String[] args) {
-//        Person person = new Person()
-//        person.firstName = "Nader"
-//        person.lastName = "Althubaity"
-//        person.setAge(50)
-//        println(person.getName())
 
         def persons = [
                 new Person(firstName: "Tamara" , lastName: "Sud" , age: 23),
@@ -13,18 +10,14 @@ class AST {
                 new Person(firstName: "Alejandra" , lastName: "Rivera" , age:  28)
         ]
 
-        //Iterate over persons array
 
-        for (Person p : persons){
-//            printf("Full name is %s " , p.getName())
-            if (p.getAge() > 20 && p.getAge() < 40){
-                println(" Full name is: " + p.getName()
-                        +" You are "+ p.getAge() +" and you are young ")
-            }
-            else {
-                println(" Full name is: " + p.getName()
-                        + " your age is " + p.getAge() + ":((")
-            }
+        // The exception is handled magically
+
+        try {
+            println(persons[1].getName().toLong())
+        }
+        catch (Exception e){
+            println(e.message)
         }
     }
 }
