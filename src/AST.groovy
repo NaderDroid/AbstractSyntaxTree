4c9739ebedfd7dc5f03db6e4f18cef075b233451
Nader <na511dr@hotmail.com>
2021-01-29 19:37:04 +0300

    conditionals in groovy

--- src/AST.groovy
+++ src/AST.groovy
@@ -3,6 +3,14 @@ class AST {
         Person person = new Person()
         person.firstName = "Nader"
         person.lastName = "Althubaity"
+        person.setAge(50)
         println(person.getName())
+
+        if (person.getAge() > 20 && person.getAge() < 40){
+            printf("You are %s and you are young" , person.getAge())
+        }
+        else {
+            printf("your age is %s" , person.getAge())
+        }
     }
 }
