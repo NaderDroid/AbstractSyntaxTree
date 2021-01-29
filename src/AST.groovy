e5fb3ac5ebc07caf2b3f8ee500c751b60ccec907
Nader <na511dr@hotmail.com>
2021-01-29 20:13:24 +0300

    iteratives in groovy

--- src/AST.groovy
+++ src/AST.groovy
@@ -1,16 +1,30 @@
 class AST {
     static void main(String[] args) {
-        Person person = new Person()
-        person.firstName = "Nader"
-        person.lastName = "Althubaity"
-        person.setAge(50)
-        println(person.getName())
+//        Person person = new Person()
+//        person.firstName = "Nader"
+//        person.lastName = "Althubaity"
+//        person.setAge(50)
+//        println(person.getName())
 
-        if (person.getAge() > 20 && person.getAge() < 40){
-            printf("You are %s and you are young" , person.getAge())
-        }
-        else {
-            printf("your age is %s" , person.getAge())
+        def persons = [
+                new Person(firstName: "Tamara" , lastName: "Sud" , age: 23),
+                new Person(firstName: "Yuri" , lastName: "Ueno" , age: 30),
+                new Person(firstName: "Nader" , lastName: "Halulu" , age:  50),
+                new Person(firstName: "Alejandra" , lastName: "Rivera" , age:  28)
+        ]
+
+        //Iterate over persons array
+
+        for (Person p : persons){
+//            printf("Full name is %s " , p.getName())
+            if (p.getAge() > 20 && p.getAge() < 40){
+                println(" Full name is: " + p.getName()
+                        +" You are "+ p.getAge() +" and you are young ")
+            }
+            else {
+                println(" Full name is: " + p.getName()
+                        + " your age is " + p.getAge() + ":((")
+            }
         }
     }
 }
--- src/Person.groovy
+++ src/Person.groovy
@@ -2,7 +2,7 @@ class Person {
     String firstName;
     String lastName;
     int age;
-    String fullName = lastName + ", "+ firstName;
+  //  String fullName = lastName + ", "+ firstName;
 
     String getName(){
         return lastName + ", "+ firstName;
