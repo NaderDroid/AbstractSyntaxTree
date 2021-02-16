eddb4c1319eae60fd483546119712b403e1eb089
Nader <na511dr@hotmail.com>
2021-02-17 01:38:52 +0300

    AST implementation for default ToString

--- src/AST.groovy
+++ src/AST.groovy
@@ -6,25 +6,9 @@ class AST {
 //        person.setAge(50)
 //        println(person.getName())
 
-        def persons = [
-                new Person(firstName: "Tamara" , lastName: "Sud" , age: 23),
-                new Person(firstName: "Yuri" , lastName: "Ueno" , age: 30),
-                new Person(firstName: "Nader" , lastName: "Halulu" , age:  50),
-                new Person(firstName: "Alejandra" , lastName: "Rivera" , age:  28)
-        ]
+        Person person = new Person();
+        Person person1 = new Person("Nader" , "Althubaity" , 19);
 
-        //Iterate over persons array
-
-        for (Person p : persons){
-//            printf("Full name is %s " , p.getName())
-            if (p.getAge() > 20 && p.getAge() < 40){
-                println(" Full name is: " + p.getName()
-                        +" You are "+ p.getAge() +" and you are young ")
-            }
-            else {
-                println(" Full name is: " + p.getName()
-                        + " your age is " + p.getAge() + ":((")
-            }
-        }
+        println(person1.toString())
     }
 }
