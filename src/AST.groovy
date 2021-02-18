721aa9c31370d4b8c22ef73774c6efe21d4691ea
Nader <na511dr@hotmail.com>
2021-02-18 04:11:02 +0300

    AST implementation for default ToString

--- src/AST.groovy
+++ src/AST.groovy
@@ -6,9 +6,36 @@ class AST {
 //        person.setAge(50)
 //        println(person.getName())
 
-        Person person = new Person();
-        Person person1 = new Person("Nader" , "Althubaity" , 19);
+        Person person = new Person("Nader" , "Althubaity" , 19);
+        Person person1 = new Person("Nader1" , "Althubaity1" , 191);
+        Person person3 = new Person("Yuri" , "Althubaity1" , 123);
 
-        println(person1.toString())
+
+        Closure personToString = { println(person.toString()) }
+        Closure dynamicPersonToString = { Person person2 -> println(person2.toString()) }
+
+
+//        personToString();
+//        dynamicPersonToString(person1);
+
+        handler(dynamicPersonToString , person)
+
+        //Print user full name without toString method
+        //This is good in the case that users can avoid using ToString for any reason
+        //Also extra liability for using user defined methods
+
+        Closure printFullName = { Person person2 -> println(person2.firstName
+        +" " + person2.lastName)}
+        Person person2;
+        handler(printFullName , person2)
+    }
+
+    static void handler(Closure closure , Person person) throws NullPointerException{
+        try {
+            closure(person)
+        }
+        catch (NullPointerException e){
+            println("("+e.getMessage() + ")\n Make sure Person instance is defined")
+        }
     }
 }
